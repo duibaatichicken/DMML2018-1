77d861081ff5662fe1eb27e9bc0d89ae89e6d5c8
sahil <sahil.sahil.sahil@gmail.com>
2018-09-15 00:58:28 +0530

    Classifier Addition: Naive Bayesian Classifier.
    - Added class for the Naive Bayesian Classifier.

---
+++ src/c4classifiers/NaiveBayesianClassifier.java
@@ -0,0 +1,188 @@
+package c4classifiers;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This class contains a classifier for connect4 data
+ * using a naive Bayesian approach.
+ *
+ */
+public class NaiveBayesianClassifier {
+
+	/**
+	 * The maps contains all probabilities. The counts map contains the
+	 * conditional probabilities, given that the result is 'win', 'draw',
+	 * or 'loss'. Each of these has a 42 X 3 array of probabilities. For
+	 * each array, the [i][j]th element is the probability that the ith
+	 * attribute has value j.
+	 * The convention for second component is as follows:
+	 * 0 - 'b' - 'win'
+	 * 1 - 'o' - 'draw'
+	 * 2 - 'x' - 'loss'
+	 * Examples are as follows:
+	 * 
+	 * 1) countGiven.get("win")[4][0] represents the probability that the 5th
+	 * attribute is a 'b' given the result is a "win"
+	 * 
+	 * 2) countGiven.get("draw")[36][2] represents the probability that the 37th
+	 * attribute is an 'x' given the result is a "loss"
+	 *
+	 */
+	private Map<String, int[][]> countsGiven;
+	private Map<String, Integer> resultCountMap;
+	private int totalCount;
+	
+	private static int ATTRIBUTE_SIZE = 42;
+	private static String TRAINING_DATA_SOURCE = "src/data/connect4-cv_training_data-10-01.data";
+	private static String TESTING_DATA_SOURCE = "src/data/connect4-cv_test_data-10-01.data";
+	
+	/************************* *************************/
+	
+	/**
+	 * Constructor
+	 * @throws IOException 
+	 */
+	public NaiveBayesianClassifier() throws IOException {
+		
+		this.countsGiven = new HashMap<String, int[][]>();
+		countsGiven.put("win", new int[ATTRIBUTE_SIZE][3]);
+		countsGiven.put("draw", new int[ATTRIBUTE_SIZE][3]);
+		countsGiven.put("loss", new int[ATTRIBUTE_SIZE][3]);
+		
+		// Create instances of probability arrays.
+		this.totalCount = 0;
+		
+		// Initialise all probabilities to zero.
+		for(int i=0;i<ATTRIBUTE_SIZE;++i) {
+			for(int j=0;j<3;++j) {
+				this.countsGiven.get("win")[i][j] = 0;
+				this.countsGiven.get("draw")[i][j] = 0;
+				this.countsGiven.get("loss")[i][j] = 0;
+			}
+		}
+		
+		// Create instance of result probability array.
+		this.resultCountMap = new HashMap<String, Integer>();
+		resultCountMap.put("win", 0);
+		resultCountMap.put("draw", 0);
+		resultCountMap.put("loss", 0);
+		
+		// Populate probability arrays by reading data source.
+		buildCountsArrays();
+	}
+	
+	/************************* *************************/
+	
+	/**
+	 * @throws IOException 
+	 * @description Compiles a list of probabilities based on
+	 * reading the data source file.
+	 */
+	private void buildCountsArrays() throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(TRAINING_DATA_SOURCE));
+		String currLine = "";
+		while((currLine = br.readLine()) != null) {
+			String[] tmpArray = currLine.split(",");
+			int lineSize = tmpArray.length;
+			String result = tmpArray[lineSize-1]; // win, draw, loss
+			for(int i=0;i<lineSize-1;++i) {
+				if(tmpArray[i].equals("b")) {
+					countsGiven.get(result)[i][0]++;
+				} else if(tmpArray[i].equals("o")) {
+					countsGiven.get(result)[i][1]++;
+				} else if(tmpArray[i].equals("x")) {
+					countsGiven.get(result)[i][2]++;
+				} else {
+					throw(new RuntimeException("Invalid data!"));
+				}
+			}
+			resultCountMap.put(result, resultCountMap.get(result)+1);
+		}
+		br.close();
+		totalCount = resultCountMap.get("win") + resultCountMap.get("draw") + resultCountMap.get("loss");
+	}
+	
+	/************************* *************************/
+	
+	/**
+	 * @description Returns the specified probability. For explanation
+	 * on convention of values, see comment before the definition of
+	 * the probability array.
+	 */
+	public double getProbability(int attribute, int value, String given) {
+		if(attribute < 0 || attribute > 41) {
+			throw(new RuntimeException("Attribute index out of bounds!"));
+		} else {
+			if(value < 0 || value > 2) {
+				throw(new RuntimeException("Value index out of bounds!"));
+			} else {
+				return (double)countsGiven.get(given)[attribute][value] / (double) resultCountMap.get(given);
+			}
+		}
+	}
+	
+	/************************* *************************/
+	
+	/**
+	 * @description Classify a line of input based on the Bayesian
+	 * probabilities built earlier. The line is assumed to be formatted
+	 * the same as training data, except for the last, result field.
+	 */
+	public String classify(String line) {
+		String[] tmpArray = line.split(",");
+		int lineSize = tmpArray.length;
+		double[] prob = new double[3];
+		prob[0] = 1; prob[1] = 1; prob[2] = 1;
+		for(int i=0;i<lineSize-1;++i) {
+			if(tmpArray[i].equals("b")) {
+				prob[0] *= getProbability(i, 0, "win");
+				prob[1] *= getProbability(i, 0, "draw");
+				prob[2] *= getProbability(i, 0, "loss");
+			} else if(tmpArray[i].equals("o")) {
+				prob[0] *= getProbability(i, 1, "win");
+				prob[1] *= getProbability(i, 1, "draw");
+				prob[2] *= getProbability(i, 1, "loss");
+			} else if(tmpArray[i].equals("x")) {
+				prob[0] *= getProbability(i, 2, "win");
+				prob[1] *= getProbability(i, 2, "draw");
+				prob[2] *= getProbability(i, 2, "loss");
+			} else {
+				throw(new RuntimeException("Invalid data line!"));
+			}
+		}
+		prob[0] *= ((double)resultCountMap.get("win") / (double) totalCount);
+		prob[1] *= ((double)resultCountMap.get("draw") / (double) totalCount);
+		prob[2] *= ((double)resultCountMap.get("loss") / (double) totalCount);
+		
+		String ans = "";
+		if(prob[0] >= prob[1]) {
+			if(prob[2] >= prob[0]) {
+				ans = "loss";
+			} else {
+				ans = "win";
+			}
+		} else { // prob[0] < prob[1]
+			if(prob[2] >= prob[1]) {
+				ans = "loss";
+			} else {
+				ans = "draw";
+			}
+		}
+		return ans;
+	}
+	
+	/************************* *************************/
+	
+	/**
+	 * @throws IOException 
+	 * @description Main function for local testing.
+	 */
+	@SuppressWarnings("unused")
+	public static void main(String[] args) throws IOException {
+		NaiveBayesianClassifier nbc = new NaiveBayesianClassifier();
+	}
+}
