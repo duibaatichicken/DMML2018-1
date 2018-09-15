d92b7176fe8cd914653b935c2fde5e7748737022
duibaatichicken <as7.gg4friends@gmail.com>
2018-09-16 04:42:13 +0530

    took some helper funtions into util feel free to generalize said class also gitignore now includes *.swp files

---
+++ src/util/HelperFunctionsDazzle.java
@@ -0,0 +1,37 @@
+package util;
+public class HelperFunctionsDazzle
+{
+    /************************* *************************/   
+    
+    /**
+     * @description helper function to convert b,o,x to numbers
+     */
+    public static int attrToInt(String attr)
+    {
+	if (attr.equals("b"))
+	    return 0;
+	else if (attr.equals("o"))
+	    return 1;
+	else if (attr.equals("x"))
+	    return 2;
+	else
+	    throw(new RuntimeException("Invalid attribute value"));
+    }
+    /************************* *************************/   
+    
+    /**
+     * @description helper function to convert classes to numbers
+     */
+    public static int classToInt(String cl)
+    {
+	if (cl.equals("draw"))
+	    return 0;
+	else if (cl.equals("win"))
+	    return 1;
+	else if (cl.equals("loss"))
+	    return 2;
+	else
+	    throw (new RuntimeException("Invalid class"));
+    }
+    
+}
