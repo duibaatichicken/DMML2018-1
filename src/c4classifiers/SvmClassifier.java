dcb5ac289cd32f47a9ee594798b056ac90042088
sahil <sahil.sahil.sahil@gmail.com>
2018-09-16 13:22:14 +0530

    Class Addition: SVM Classifier.
    - Added pre-processing code for the SVM classifier.

---
+++ src/c4classifiers/SvmClassifier.java
@@ -0,0 +1,56 @@
+package c4classifiers;
+
+import util.StaticConstants;
+
+/**
+ * This class provides a classifier for the Connect-4
+ * data using Support Vector Machines.
+ *
+ */
+public class SvmClassifier {
+
+	/************************* *************************/
+	
+	/**
+	 * Constructor
+	 */
+	public SvmClassifier() {
+		
+	}
+	
+	/************************* *************************/
+	
+	/**
+	 * @description Accepts a line formatted like the Connect-4
+	 * data and converts it to a array of integers. The convention
+	 * followed is as follows
+	 * 0 - b (blank position on the board)
+	 * +1 - x (counters played by Player 1)
+	 * -1 - o (counters played by Player 2)
+	 */
+	private int[] convertToPoint(String line) {
+		int[] ans = new int[42];
+		String[] tmpArray = line.split(",");
+		for(int i=0;i<StaticConstants.CLASS_COLUMN;++i) {
+			if(tmpArray[i].equals("b")) {
+				ans[i] = 0;
+			} else if(tmpArray[i].equals("o")) {
+				ans[i] = -1;
+			} else if(tmpArray[i].equals("x")) {
+				ans[i] = 1;
+			} else {
+				throw(new RuntimeException("Invalid data!"));
+			}
+		}
+		return ans;
+	}
+	
+	/************************* *************************/
+	
+	/**
+	 * @description Main function for local testing.
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
